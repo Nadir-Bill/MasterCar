6ad9833cc170c551bcfa73e10c78cc1d5ea2d476
AmineBenz <medbenzagouta@gmail.com>
2020-02-11 06:24:30 +0100

    # commit 3 login and requestQueue

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.example.myapplication.requests.MasterCarService;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -9,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,8 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,8 +28,28 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                // i do not know who put this here
             }
         });
+
+        Button loginButton = findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "wow clicked", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                EditText login = findViewById(R.id.loginField);
+                EditText pwd    = findViewById(R.id.PasswordField);
+
+                // TODO execute login
+
+                MasterCarService mcService = new MasterCarService();
+                mcService.sendRequest(view.getContext(), MasterCarService.testUrl);
+            }
+        });
+
+
     }
 
 }
